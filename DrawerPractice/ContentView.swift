4391f5e1454f91899627819ee34454f578118750
EhsanTgv <EhsanTgv@gmail.com>
2022-08-04 10:49:00 +0300

    Implement Simple Drawer

--- DrawerPractice/ContentView.swift
+++ DrawerPractice/ContentView.swift
@@ -5,17 +5,34 @@
 //  Created by Ehsan Taghavi on 4.08.2022.
 //
 
+import Drawer
 import SwiftUI
 
 struct ContentView: View {
+    @State var heights = [CGFloat(300), CGFloat(100)]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                ScrollView{
+                    Text("Some content in here")
+                }
+                Drawer(heights: $heights){
+                    Color.blue
+                }
+            }
+            .navigationTitle("Drawer Practice")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewDevice("iPhone 13 Pro")
+
+        }
     }
 }
