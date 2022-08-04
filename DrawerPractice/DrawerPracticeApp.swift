3923c995d95ba743bf26d7f7eb348d1aa98f9a98
EhsanTgv <EhsanTgv@gmail.com>
2022-08-04 10:29:16 +0300

    Initial Commit

---
+++ DrawerPractice/DrawerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerPracticeApp.swift
+//  DrawerPractice
+//
+//  Created by Ehsan Taghavi on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
